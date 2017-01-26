39cef4e44ed67ac7bf37fc4257395ab83d8e91b3
josu1502 <josu1502@student.miun.se>
2017-01-26 14:48:48 +0100

    Testar kollidera

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -17,7 +17,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        System.out.println("heyo");
+        System.out.println("Mayo");
 
         System.out.println("How are you today?");
     }
