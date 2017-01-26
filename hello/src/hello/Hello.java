0373b14b2283a2a2577b8114bdaa23c6ea2a2203
mavn1500 <mvangmans@hotmail.com>
2017-01-26 14:49:52 +0100

    test2

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -17,7 +17,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        System.out.println("heyo");
+        System.out.println("anders");
 
         System.out.println("How are you today?");
     }
