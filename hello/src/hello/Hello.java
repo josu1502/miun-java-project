6d67196de8c6877a8b2db645d222bfc2c0c0a723
josu1502 <josu1502@student.miun.se>
2017-01-26 14:24:56 +0100

    Lagt till en ny rad System.out

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -16,6 +16,7 @@ public class Hello {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("How are you today?");
     }
     
 }
