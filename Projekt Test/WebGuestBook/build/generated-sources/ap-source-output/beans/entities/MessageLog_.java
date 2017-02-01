d63bdbcfb8d8496be9bd967f0b72b69eda9c197d
josu1502 <josu1502@student.miun.se>
2017-02-01 15:58:50 +0100

    Added new test project

---
+++ Projekt Test/WebGuestBook/build/generated-sources/ap-source-output/beans/entities/MessageLog_.java
@@ -0,0 +1,15 @@
+package beans.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-31T16:41:42")
+@StaticMetamodel(MessageLog.class)
+public class MessageLog_ { 
+
+    public static volatile SingularAttribute<MessageLog, String> name;
+    public static volatile SingularAttribute<MessageLog, Long> id;
+    public static volatile SingularAttribute<MessageLog, String> message;
+
+}
---
+++ Projekt Test/WebGuestBook/src/java/beans/MessageMethods.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import beans.entities.MessageLog;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author Joakim
+ */
+@Named(value = "messageMethods")
+@SessionScoped
+public class MessageMethods implements Serializable {
+
+    @PersistenceContext(unitName = "WebGuestBookPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+
+    /**
+     * Creates a new instance of MessageMethods
+     */
+    public MessageMethods() {
+    }
+    
+    public void sendMessage(String name, String message) {
+        MessageLog msglog = new MessageLog();
+        msglog.setName(name);
+        msglog.setMessage(message);
+        
+        persist(msglog);
+    }
+    
+    public String getMessages() {
+        TypedQuery<MessageLog> q = em.createNamedQuery("getAll", MessageLog.class);
+        List<MessageLog> resultList = q.getResultList();
+        
+        String allMessages = "";
+        
+        for (int i = 0; i < resultList.size(); i++) {
+            MessageLog nameAndMessage = resultList.get(i);
+            allMessages += nameAndMessage.getName();
+            allMessages += ": ";
+            allMessages += nameAndMessage.getMessage();
+            allMessages += "\n";
+            //nameAndMessage.
+            //allMessages += System.getProperty("line.Seperator");
+        }
+        
+        return allMessages;
+    }
+
+    public void persist(Object object) {
+        try {
+            utx.begin();
+            em.persist(object);
+            utx.commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
+    }
+    
+}
