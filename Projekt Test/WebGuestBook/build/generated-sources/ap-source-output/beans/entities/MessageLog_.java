0001387df50b834cf1abd2a40eec8998b597db29
aned1602 <andreas.edin@yahoo.com>
2017-02-03 10:33:16 +0100

    Krånglar, försöker dra ner filerna...

--- Projekt Test/WebGuestBook/build/generated-sources/ap-source-output/beans/entities/MessageLog_.java
+++ Projekt Test/WebGuestBook/build/generated-sources/ap-source-output/beans/entities/MessageLog_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-31T16:41:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-02-02T14:30:21")
 @StaticMetamodel(MessageLog.class)
 public class MessageLog_ { 
 
