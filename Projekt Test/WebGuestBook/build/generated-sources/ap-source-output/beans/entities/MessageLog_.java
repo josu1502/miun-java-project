acf83e15f244ce31f9a699c61a11560879f3eb4a
josu1502 <josu1502@student.miun.se>
2017-02-06 16:33:29 +0100

    La till ett projekt för android studio

--- Projekt Test/WebGuestBook/build/generated-sources/ap-source-output/beans/entities/MessageLog_.java
+++ Projekt Test/WebGuestBook/build/generated-sources/ap-source-output/beans/entities/MessageLog_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-02-04T11:51:34")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-02-06T16:04:40")
 @StaticMetamodel(MessageLog.class)
 public class MessageLog_ { 
 
