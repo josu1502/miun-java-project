e809ba86c18640c5cac8821d49ff7d781ff1a0c3
josu1502 <josu1502@student.miun.se>
2017-02-08 13:02:11 +0100

    Unkown change

--- Projekt Test/AppGuestBook/app/src/main/java/com/miun/appguestbook/josu1502/appguestbook/MainActivity.java
+++ Projekt Test/AppGuestBook/app/src/main/java/com/miun/appguestbook/josu1502/appguestbook/MainActivity.java
@@ -33,7 +33,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
         restMessageClient.requestMessages();
     }
 
