22aa844400332a11162b66bb1fb3059cc0b7aa7a
josu1502 <josu1502@student.miun.se>
2017-02-14 12:50:14 +0100

    La till en jävla massa funktioner i lunch sessionsbönan

--- Projekt/AntonsHemsida/src/java/beans/LunchSessionBean.java
+++ Projekt/AntonsHemsida/src/java/beans/LunchSessionBean.java
@@ -9,6 +9,7 @@ import beans.entities.LunchEntity;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,6 +19,11 @@ import javax.persistence.FlushModeType;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
 
 /**
  *
@@ -31,46 +37,152 @@ public class LunchSessionBean implements Serializable {
     private EntityManager em;
     @Resource
     private javax.transaction.UserTransaction utx;
-    //Ex på ändra i en databas: https://www.mkyong.com/jsf2/how-to-update-row-in-jsf-datatable/
+
+    private class LunchStruct {
+
+        public String day;
+        public String name;
+        public String desc;
+        public Integer price;
+    }
+    private List<LunchStruct> lunchList = new ArrayList<>();
+
     /**
      * Creates a new instance of LunchSessionBean
      */
     public LunchSessionBean() {
-    }
-    
-    public void setlunch() {
-        /*TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class);
-        List<LunchEntity> resultList = q.getResultList();
-        
-        LunchEntity newLunch = resultList.get(0);
-        newLunch.setName("Pannkakor 2");
-        newLunch.setDescription("Med grädde och sylt 2");
-        newLunch.setPrice(82);
-        
-        resultList.set(0, newLunch);
-        
-        em.merge(resultList);*/
-        
-        LunchEntity lunchname = new LunchEntity();
-        lunchname.setLunchday("Måndag");
-        lunchname.setName("Paj");
-        lunchname.setDescription("Med grädde och sylt");
-        lunchname.setPrice(82);
-        persist(lunchname);    
-    }
-    public int updateLunchPrice(Integer price, String name) {
-    Query update_query = em.createQuery("UPDATE LunchEntity p SET p.price= ?1 WHERE p.name= ?2 ");
-    update_query.setParameter(1, price);
-    update_query.setParameter(2, name);
-    //update_query.executeUpdate();
-    //em.setFlushMode(FlushModeType.COMMIT);
-    //em.close();
-    persist(update_query);
-    return 1;
-  }
-    
-    public String getlunch() {
         TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class);
+        List<LunchEntity> resultList = q.getResultList();
+
+        LunchStruct ls = new LunchStruct();
+        for (int i = 0; i < resultList.size(); i++) {
+            ls.day = resultList.get(i).getLunchday();
+            ls.name = resultList.get(i).getName();
+            ls.desc = resultList.get(i).getDescription();
+            ls.price = resultList.get(i).getPrice();
+            lunchList.set(i, ls);
+        }
+    }
+
+    public void createLunchEntity(String day, String name, String desc, Integer price) {
+        LunchEntity lunchname = new LunchEntity();
+        lunchname.setLunchday(day);
+        lunchname.setName(name);
+        lunchname.setDescription(desc);
+        lunchname.setPrice(price);
+        persist(lunchname);
+    }
+
+    public void updateAll() {
+        for (int i = 0; i < lunchList.size(); i++) {
+            updateLunchDayIndex(lunchList.get(i).day, i);
+            updateLunchNameIndex(lunchList.get(i).name, i);
+            updateLunchDescIndex(lunchList.get(i).desc, i);
+            updateLunchPriceIndex(lunchList.get(i).price, i);
+        }
+    }
+
+    public void updateLunchDayIndex(String day, Integer index) {
+        try {
+            utx.begin();
+            TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+            );
+            List<LunchEntity> resultList = q.getResultList();
+            LunchEntity updateEntity = resultList.get(index);
+            updateEntity.setLunchday(day);
+            utx.commit();
+
+        } catch (Exception ex) {
+            Logger.getLogger(LunchSessionBean.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updateLunchNameIndex(String name, Integer index) {
+        try {
+            utx.begin();
+            TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+            );
+            List<LunchEntity> resultList = q.getResultList();
+            LunchEntity updateEntity = resultList.get(index);
+            updateEntity.setName(name);
+            utx.commit();
+
+        } catch (Exception ex) {
+            Logger.getLogger(LunchSessionBean.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updateLunchDescIndex(String desc, Integer index) {
+        try {
+            utx.begin();
+            TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+            );
+            List<LunchEntity> resultList = q.getResultList();
+            LunchEntity updateEntity = resultList.get(index);
+            updateEntity.setDescription(desc);
+            utx.commit();
+
+        } catch (Exception ex) {
+            Logger.getLogger(LunchSessionBean.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updateLunchPriceIndex(Integer price, Integer index) {
+        try {
+            utx.begin();
+            TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+            );
+            List<LunchEntity> resultList = q.getResultList();
+            LunchEntity updateEntity = resultList.get(index);
+            updateEntity.setPrice(price);
+            utx.commit();
+
+        } catch (Exception ex) {
+            Logger.getLogger(LunchSessionBean.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String
+            readLunchDayIndex(Integer index) {
+        TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+        );
+        List<LunchEntity> resultList = q.getResultList();
+        return resultList.get(index).getLunchday();
+    }
+
+    public String
+            readLunchNameIndex(Integer index) {
+        TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+        );
+        List<LunchEntity> resultList = q.getResultList();
+        return resultList.get(index).getName();
+    }
+
+    public String
+            readLunchDescIndex(Integer index) {
+        TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+        );
+        List<LunchEntity> resultList = q.getResultList();
+        return resultList.get(index).getDescription();
+    }
+
+    public Integer
+            readLunchPriceIndex(Integer index) {
+        TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+        );
+        List<LunchEntity> resultList = q.getResultList();
+        return resultList.get(index).getPrice();
+    }
+
+    /*display... bygger html kod*/
+    public String
+            displayLunchMenu() {
+        TypedQuery<LunchEntity> q = em.createNamedQuery("getAll", LunchEntity.class
+        );
         List<LunchEntity> resultList = q.getResultList();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < resultList.size(); i++) {
@@ -80,22 +192,20 @@ public class LunchSessionBean implements Serializable {
             sb.append("</h2>");
             sb.append("<h3>");
             sb.append(nameAndMessage.getName());
-            sb.append(" "); 
+            sb.append(" ");
             sb.append(nameAndMessage.getPrice());
             sb.append(":-");
             sb.append("</h3>");
             sb.append("<p>");
             sb.append(nameAndMessage.getDescription());
             sb.append("</p>");
-            
+
         }
         return sb.toString();
     }
-    
-    /*Hämta lunchmeny*/
-    
-    /*Hämta lunch på index*/
 
+    /*Hämta lunchmeny*/
+ /*Hämta lunch på index*/
     public void persist(Object object) {
         try {
             utx.begin();
@@ -106,7 +216,38 @@ public class LunchSessionBean implements Serializable {
             throw new RuntimeException(e);
         }
     }
-    
-    
-    
+
+    /*Getter and setter for this object, not the entity. Used in textfields.*/
+    public String getDay(Integer index) {
+        return lunchList.get(index).day;
+    }
+
+    public void setDay(String day, Integer index) {
+        this.lunchList.get(index).day = day;
+    }
+
+    public String getName(Integer index) {
+        return lunchList.get(index).name;
+    }
+
+    public void setName(String name, Integer index) {
+        this.lunchList.get(index).name = name;
+    }
+
+    public String getDesc(Integer index) {
+        return lunchList.get(index).desc;
+    }
+
+    public void setDesc(String desc, Integer index) {
+        this.lunchList.get(index).desc = desc;
+    }
+
+    public Integer getPrice(Integer index) {
+        return lunchList.get(index).price;
+    }
+
+    public void setPrice(Integer price, Integer index) {
+        this.lunchList.get(index).price = price;
+    }
+
 }
