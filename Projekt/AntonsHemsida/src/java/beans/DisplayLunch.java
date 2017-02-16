56c8236c916c34463b069bda88c30e174f2247e1
thas1500 <thas1500@student.miun.se>
2017-02-16 13:04:55 +0100

    lite changes

--- Projekt/AntonsHemsida/src/java/beans/DisplayLunch.java
+++ Projekt/AntonsHemsida/src/java/beans/DisplayLunch.java
@@ -10,6 +10,7 @@ import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,7 +31,8 @@ public class DisplayLunch implements Serializable {
     private EntityManager em;
     @Resource
     private javax.transaction.UserTransaction utx;
-
+    
+    
     public DisplayLunch() {
     }
 
@@ -51,4 +53,50 @@ public class DisplayLunch implements Serializable {
         }
         return sb.toString();
     }
+    
+    public String displayLunchToday() {
+        
+    
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        String currentDay;
+        if(day == 2){
+            currentDay = "Måndag";
+        }
+        else if(day == 3){
+            currentDay = "Tisdag";
+        }
+        else if(day == 4){
+            currentDay = "Onsdag";
+        }
+        else if(day == 5){
+            currentDay = "Torsdag";
+        }
+        else if(day == 6){
+            currentDay = "Fredag";
+        }
+        else if(day == 7){
+            currentDay = "Lördag";
+        }
+        else {
+            currentDay = "Söndag";
+        }
+        TypedQuery<LunchEntity> tq = em.createNamedQuery("getDay", LunchEntity.class).setParameter("weekday", currentDay);
+        List<LunchEntity> resultlist = tq.getResultList();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < resultlist.size(); i++) {
+            LunchEntity nameAndMessage = resultlist.get(i);
+
+            sb.append("<h3>");
+            sb.append(nameAndMessage.getName());
+            sb.append(" ");
+            sb.append(nameAndMessage.getPrice());
+            sb.append(":-");
+            sb.append("</h3>");
+            sb.append(nameAndMessage.getDescription());
+        }
+        return sb.toString();
+
+    }
+
 }
