567e640cfb91bc2bba0e7d8eaa8af10237432f3e
aned1602 <andreas.edin@yahoo.com>
2017-03-01 09:57:43 +0100

    NY entitetsböna (OrderEntity) skapad, dessutom tillhörande REST-ful web service (OrderEntityFacadeREST)

---
+++ Projekt/AntonsHemsida/src/java/beans/entities/OrderEntity.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author and12edi
+ */
+@Entity
+@XmlRootElement
+public class OrderEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Integer tableNr;
+    private String courseName;
+    private String courseType;
+    private Integer amount;
+    private Boolean finished;
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OrderEntity)) {
+            return false;
+        }
+        OrderEntity other = (OrderEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "beans.entities.OrderEntity[ id=" + id + " ]";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @return the tableNr
+     */
+    public Integer getTableNr() {
+        return tableNr;
+    }
+
+    /**
+     * @param tableNr the tableNr to set
+     */
+    public void setTableNr(Integer tableNr) {
+        this.tableNr = tableNr;
+    }
+
+    /**
+     * @return the courseName
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /**
+     * @param courseName the courseName to set
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    /**
+     * @return the courseType
+     */
+    public String getCourseType() {
+        return courseType;
+    }
+
+    /**
+     * @param courseType the courseType to set
+     */
+    public void setCourseType(String courseType) {
+        this.courseType = courseType;
+    }
+
+    /**
+     * @return the amount
+     */
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return the finished
+     */
+    public Boolean getFinished() {
+        return finished;
+    }
+
+    /**
+     * @param finished the finished to set
+     */
+    public void setFinished(Boolean finished) {
+        this.finished = finished;
+    }
+
+}
