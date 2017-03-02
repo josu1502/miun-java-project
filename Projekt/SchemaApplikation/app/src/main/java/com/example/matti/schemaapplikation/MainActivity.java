863a7e9951eb64685fe5132af80277d9d5a0b21f
josu1502 <josu1502@student.miun.se>
2017-03-02 16:49:33 +0100

    Uppdaterat loggbok

--- Projekt/SchemaApplikation/app/src/main/java/com/example/matti/schemaapplikation/MainActivity.java
+++ Projekt/SchemaApplikation/app/src/main/java/com/example/matti/schemaapplikation/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity  implements SchemaStatusList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        schemaClient = new SchemaClient("http://10.250.112.35:8080/AntonsHemsida/webresources/"); /* Jocke skola IP: */
+        schemaClient = new SchemaClient("http://10.250.115.39:8080/AntonsHemsida/webresources/"); /* Jocke skola IP: */
         //schemaClient = new SchemaClient("http://192.168.0.106:8080/AntonsHemsida/webresources/"); /* Jocke hemma IP: */
         schemaClient.setStatusListener(this);
         schemaClient.fetchSchemaList();
