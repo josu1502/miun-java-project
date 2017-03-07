7cb385250ecbab948033581a53e9bdfc385d9a1b
aned1602 <andreas.edin@yahoo.com>
2017-03-07 17:25:10 +0100

    Pushar upp Andreas och Thomas jobb med att kunna vis aposters för kommande spelningar (ännu ej fungerande)

---
+++ Projekt/AntonsHemsida/src/java/beans/DisplayEntertainment.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import db.Image;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author and12edi
+ */
+@Named(value = "displayEntertainment")
+@SessionScoped
+public class DisplayEntertainment implements Serializable {
+
+    @PersistenceContext(unitName = "AntonsHemsidaPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+
+    public DisplayEntertainment() {
+    }
+
+    public String displayAllPosters() {
+        TypedQuery<Image> tq = em.createNamedQuery("Image.selectAll", Image.class);
+        List<Image> resultList = tq.getResultList();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < resultList.size(); i++) {
+            Image posterAndDescription = resultList.get(i);
+            sb.append("<h1>Kommande spelningar på Antons</h1>");
+            sb.append("<br/>");
+            sb.append("<img width='400px' height='600px' src='/Images/'");
+            sb.append(posterAndDescription.getUrl());
+            sb.append("alt='poster is missing'></img>");
+            sb.append("<br/>");
+        }
+        return sb.toString();
+    }
+}
