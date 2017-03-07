1e15e46489c11518490b88dcc3aa0bded929d372
thas1500 <thas1500@student.miun.se>
2017-03-07 15:36:33 +0100

    Arbetet med bilderna forsätter, det går nu att ladda upp bilder så de syns direkt efter ett möte med martin

---
+++ Projekt/AntonsHemsida/src/java/beans/form/ImageList.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans.form;
+
+import db.Image;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author maski
+ */
+@Named(value = "imageList")
+@RequestScoped
+public class ImageList {
+
+    @PersistenceContext(unitName = "AntonsHemsidaPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+
+    
+    public ImageList() {
+    }
+
+    public List<Image> getImages() {
+        TypedQuery<Image> selectAllImages = em.createNamedQuery("Image.selectAll", Image.class);
+        return selectAllImages.getResultList();
+    }
+    
+}
--- Projekt/AntonsHemsida/src/java/db/Image.java
+++ Projekt/AntonsHemsida/src/java/db/Image.java
@@ -5,9 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
-@NamedQuery(name = "Image.selectAll", query = "SELECT img FROM Image img")
+@NamedQueries({
+    @NamedQuery(name = "Image.selectAll", query = "SELECT img FROM Image img"),
+    @NamedQuery(name = "Image.removeById", query = "DELETE FROM Image img WHERE img.id= :IMGID")     
+})
 @Entity
 
 public class Image implements Serializable {
@@ -18,46 +22,53 @@ public class Image implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    /**
-     * @return the description
-     */
     public String getDescription() {
         return description;
     }
-
-    /**
-     * @param description the description to set
-     */
+    
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * @return the url
-     */
     public String getUrl() {
         return url;
     }
 
-    /**
-     * @param url the url to set
-     */
     public void setUrl(String url) {
         this.url = url;
     }
 
-    /**
-     * @return the id
-     */
     public Long getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(Long id) {
         this.id = id;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode():0);
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        //TODO: warning-this method wont work in the case the id fields are not set
+        if(!(object instanceof Image)) {
+            return false;
+        }
+        Image other = (Image) object;
+        if((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "db.Images[ id=" + id + " ]";
     }
     
 }
